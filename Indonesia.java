7d8870ac7e3f8f755fd749a733c23c16a39754cd
unknown <75077337+noersinta@users.noreply.github.com>
2021-03-21 23:17:16 +0700

    Create Indonesia.java

---
+++ Indonesia.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package indonesia;
+
+/**
+ *
+ * @author Niezza
+ */
+public class Indonesia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                String nama = "Indonesia";
+        char abjad = 'a';
+        int a = 6;
+        double b = 6.9;
+        
+        System.out.println (nama);
+        System.out.println (a);       
+
+    }
+    
+}
